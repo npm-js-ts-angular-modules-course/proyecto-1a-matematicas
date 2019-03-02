57d456e9b9fb7300d9d8e8a1fb321bd6cc3dea65
Anartz Mugika <mugan86@gmail.com>
2019-03-02 18:43:04 +0100

    Funcionalidad para validar numeros OK

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la suma
      */
     suma: function (n1, n2) {
-        return n1 + n2;
+        return (this.esNumero(n1, n2) ? n1 + n2: this.mensajeError());
     },
     /**
      * Resta de dos numeros
@@ -17,7 +17,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la resta
      */
     resta: function (n1, n2) {
-        return n1 - n2;
+        return (this.esNumero(n1, n2) ? n1 - n2: this.mensajeError());
     },
     /**
      * Multiplicacion de dos numeros
@@ -27,7 +27,7 @@ module.exports = {
      * @param {*} n2 Numero dos de la multiplicacion
      */
     multiplicacion: function (n1, n2) {
-        return n1 * n2;
+        return (this.esNumero(n1, n2) ? n1 * n2: this.mensajeError());
     },
     /**
      * Division de dos numeros
@@ -37,7 +37,24 @@ module.exports = {
      * @param {*} n2 Numero dos de la Division
      */
     division: function (n1, n2) {
-        return n1 / n2;
+        return (this.esNumero(n1, n2) ? n1 / n2: this.mensajeError());
+    },
+    /**
+     * Mensaje de error que se ejecuta cuando no tengamos valores numéricos
+     */
+    mensajeError: function() {
+        console.log('Un valor o los dos valores no son numéricos');
+    },
+    /**
+     * Comprueba que son valores numéricos
+     * @param {*} n1 
+     * @param {*} n2 
+     */
+    esNumero: function (n1, n2) {
+        if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+            return false;
+        }
+        return true;
     }
 }
 
